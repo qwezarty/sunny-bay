7c83174ea792c080b7de93544220ce8a0be84b04
qwezarty <qwezarty@gmail.com>
2017-11-08 20:34:25 +0800

    add @angular/flex-layout package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { BootComponent } from './boot/boot.component';
@@ -14,7 +15,8 @@ import { BootComponent } from './boot/boot.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
