af8f4bf4c4abade9fb944b3acdae3617f2beac5a
qwezarty <qwezarty@gmail.com>
2017-11-10 22:32:51 +0800

    outline room-list page

--- src/app/rooms/room-list/room-list.component.ts
+++ src/app/rooms/room-list/room-list.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-list.component.scss'],
 })
 export class RoomListComponent implements OnInit {
+  gridTiles = [
+    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
+    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
+    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
+    { text: 'Four', cols: 2, rows: 1, color: 'orange' },
+    { text: 'Five', cols: 2, rows: 2, color: 'lightsalmon' },
+    { text: 'Six', cols: 1, rows: 1, color: 'lightcoral' },
+    { text: 'Seven', cols: 1, rows: 1, color: 'lightyellow' },
+    { text: 'Eight', cols: 2, rows: 1, color: 'lightseagreen' },
+  ];
 
   constructor() { }
 
--- src/app/rooms/rooms.module.ts
+++ src/app/rooms/rooms.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RoomsRoutingModule } from './rooms-routing.module';
+import { MatGridListModule } from '@angular/material';
 
 import { RoomListComponent } from './room-list/room-list.component';
 import { RoomDetailComponent } from './room-detail/room-detail.component';
@@ -8,7 +9,8 @@ import { RoomDetailComponent } from './room-detail/room-detail.component';
 @NgModule({
   imports: [
     CommonModule,
-    RoomsRoutingModule
+    RoomsRoutingModule,
+    MatGridListModule
   ],
   declarations: [RoomListComponent, RoomDetailComponent]
 })
