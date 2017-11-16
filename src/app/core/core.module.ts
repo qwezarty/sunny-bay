c641e4f2535add269462382dbbacbb4b8c8cbc95
qwezarty <qwezarty@gmail.com>
2017-11-17 00:45:44 +0800

    replace app-bar with material style && remove footer for now

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule, MatButtonModule } from '@angular/material';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -10,8 +11,10 @@ import { BootComponent } from './boot/boot.component';
 @NgModule({
   imports: [
     CommonModule,
+    RouterModule,
     FlexLayoutModule,
-    RouterModule
+    MatToolbarModule,
+    MatButtonModule,
   ],
   declarations: [BootComponent, HeaderComponent, FooterComponent],
   exports: [
