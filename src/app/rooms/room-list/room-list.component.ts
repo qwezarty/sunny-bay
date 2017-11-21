7d384804ca909a06a5ecd65c38b06102d8079716
qwezarty <qwezarty@gmail.com>
2017-11-21 20:35:30 +0800

    discard grid design & code

--- src/app/rooms/room-list/room-list.component.ts
+++ src/app/rooms/room-list/room-list.component.ts
@@ -10,16 +10,6 @@ import { Observable } from 'rxjs/Observable';
 export class RoomListComponent implements OnInit {
   footerEleRef: ElementRef;
   sidenav: { 'mode': string, 'opened': string } = { 'mode': 'side', 'opened': 'true' };
-  gridTiles = [
-    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
-    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
-    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
-    { text: 'Four', cols: 2, rows: 1, color: 'orange' },
-    { text: 'Five', cols: 2, rows: 2, color: 'lightsalmon' },
-    { text: 'Six', cols: 1, rows: 1, color: 'lightcoral' },
-    { text: 'Seven', cols: 1, rows: 1, color: 'lightyellow' },
-    { text: 'Eight', cols: 2, rows: 1, color: 'lightseagreen' },
-  ];
 
   constructor(
     private breakpointService: BreakpointObserver,
