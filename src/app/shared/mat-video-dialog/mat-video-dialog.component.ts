e717349655c716f045940e6392c8a2a69ea65cbc
qwezarty <qwezarty@gmail.com>
2017-12-03 20:55:56 +0800

    add material-like video dialog

---
+++ src/app/shared/mat-video-dialog/mat-video-dialog.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Inject, ElementRef, Renderer2 } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-mat-video-dialog',
+  templateUrl: './mat-video-dialog.component.html',
+  styleUrls: ['./mat-video-dialog.component.scss'],
+})
+export class MatVideoDialogComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<MatVideoDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { title: string, src: string, poster: string },
+    private elementRef: ElementRef,
+    private render: Renderer2
+  ) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  ngOnInit() {
+    const dialogContainer = this.elementRef.nativeElement.parentElement;
+    console.log(dialogContainer);
+    this.render.setStyle(
+      dialogContainer,
+      'padding',
+      0
+    );
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 import { StylesheetService } from './stylesheet/stylesheet';
 import { ThemeService } from './theme/theme.service';
 import { ElementSrcDirective } from './element-src/element-src.directive';
+import { MatVideoDialogComponent } from './mat-video-dialog/mat-video-dialog.component';
+import { MatDialogModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatDialogModule,
+    FlexLayoutModule,
   ],
-  declarations: [ElementSrcDirective],
+  declarations: [ElementSrcDirective, MatVideoDialogComponent],
   providers: [StylesheetService],
-  exports: [ElementSrcDirective]
+  entryComponents: [MatVideoDialogComponent],
+  exports: [ElementSrcDirective, MatVideoDialogComponent]
 })
 export class SharedModule { }
